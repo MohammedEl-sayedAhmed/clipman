ace5ed528082e43c96da031c09e9b1cd5f1d2f4f
Mohammed Elsayed Ahmed <mohammedelsayed4648@gmail.com>
2026-02-10 14:52:40 +0200

    Add main GTK Application with single-instance enforcement
    Wires together database, clipboard monitor, popup window, and
    D-Bus service with graceful SIGINT/SIGTERM shutdown handling.

---
+++ clipman/app.py
@@ -0,0 +1,47 @@
+import signal
+import gi
+
+gi.require_version("Gtk", "3.0")
+from gi.repository import Gtk, GLib
+
+from clipman.database import ClipboardDB
+from clipman.clipboard_monitor import ClipboardMonitor
+from clipman.window import ClipmanWindow
+from clipman.dbus_service import ClipmanDBusService
+
+
+class ClipmanApp(Gtk.Application):
+    def __init__(self):
+        super().__init__(application_id="com.clipman.Clipman")
+        self.db = None
+        self.monitor = None
+        self.window = None
+        self.dbus_service = None
+
+    def do_activate(self):
+        if self.window:
+            self.window.toggle()
+            return
+
+        self.db = ClipboardDB()
+        self.monitor = ClipboardMonitor(self.db, on_new_entry=self._on_new_entry)
+        self.window = ClipmanWindow(self.db, self.monitor)
+        self.add_window(self.window)
+        self.dbus_service = ClipmanDBusService(self.window, self)
+        self.monitor.start()
+
+        # Handle SIGINT/SIGTERM gracefully
+        GLib.unix_signal_add(GLib.PRIORITY_DEFAULT, signal.SIGINT, self._shutdown)
+        GLib.unix_signal_add(GLib.PRIORITY_DEFAULT, signal.SIGTERM, self._shutdown)
+
+    def _on_new_entry(self):
+        if self.window and self.window.get_visible():
+            self.window.refresh()
+
+    def _shutdown(self):
+        if self.monitor:
+            self.monitor.stop()
+        if self.db:
+            self.db.close()
+        self.quit()
+        return False
